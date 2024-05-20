6b30d562604b646d8c1bd2449345cabe300ce3ba
Polyakushechka <p20090@yandex.ru>
2024-05-20 20:37:54 +0700

    Добавление второй функции

--- str_func.py
+++ str_func.py
@@ -1,3 +1,18 @@
 def foo1():
+    """
+    Функция для увеличения букв
+    :return:
+    """
     a = str(input())
     return a.capitalize()
+
+
+def foo2():
+    """
+    Функция делает первые буквы слов заглавными
+    :return:
+    """
+    b = str(input())
+    words = b.split(" ")
+    for word in words:
+        return word[0].capitalize()
